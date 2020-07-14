a09fbf40c7501a5248b0155f17f6e7aae43f486e
beyondhj@hotmail.com <beyondhj@hotmail.com>
2020-07-14 11:27:50 +0800

    可配置schema，配置分类型

--- datapipeline-core/src/main/java/org/github/datapipeline/core/factory/BatchReaderFactory.java
+++ datapipeline-core/src/main/java/org/github/datapipeline/core/factory/BatchReaderFactory.java
@@ -15,17 +15,43 @@ public class BatchReaderFactory {
 
     private static final String FORMAT = "format";
 
+    private static final String SCHEMA = "schema";
+
     public static Dataset<Row> createReader(SparkSession sparkSession, NodeData graphNode) {
         DataFrameReader dataFrameReader = sparkSession.read();
         for (Map.Entry<String, Object> entry : graphNode.getConfig().getAll().entrySet()) {
             String key = entry.getKey();
-            String value = (String) entry.getValue();
+            Object value = entry.getValue();
+            if (invalidValue(value)) {
+                continue;
+            }
             if (StringUtils.equals(key, FORMAT)) {
-                dataFrameReader.format(value);
-            } else {
-                dataFrameReader.option(key, value);
+                dataFrameReader.format((String) value);
+            } else if (StringUtils.equals(key, SCHEMA)) {
+                dataFrameReader.schema((String) value);
+            } else if (value instanceof String) {
+                dataFrameReader.option(key, (String) value);
+            } else if (value instanceof Long) {
+                dataFrameReader.option(key, (Long) value);
+            } else if (value instanceof Integer) {
+                dataFrameReader.option(key, (Integer) value);
+            } else if (value instanceof Double) {
+                dataFrameReader.option(key, (Double) value);
+            } else if (value instanceof Boolean) {
+                dataFrameReader.option(key, (Boolean) value);
             }
         }
         return dataFrameReader.load();
     }
+
+    private static boolean invalidValue(Object value) {
+        if (value == null) {
+            return true;
+        }
+        if (value instanceof String && StringUtils.isBlank((String) value)) {
+            return true;
+        }
+        return false;
+    }
+
 }
